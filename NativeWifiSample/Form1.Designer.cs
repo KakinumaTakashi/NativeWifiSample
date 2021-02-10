cce60a3dd4472ce27bff88340ef2d6c37926895f
Takashi Kakinuma <kakinuma.takashi@homes.ecweb.jp>
2021-02-10 16:08:22 +0900

    update

--- NativeWifiSample/Form1.Designer.cs
+++ NativeWifiSample/Form1.Designer.cs
@@ -29,21 +29,97 @@ namespace NativeWifiSample
         /// </summary>
         private void InitializeComponent()
         {
+            this.LabelSSID = new System.Windows.Forms.Label();
+            this.TextBoxSSID = new System.Windows.Forms.TextBox();
+            this.LabelKEY = new System.Windows.Forms.Label();
+            this.TextBoxKEY = new System.Windows.Forms.TextBox();
+            this.ButtonConnect = new System.Windows.Forms.Button();
+            this.ButtonDisconnect = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // LabelSSID
+            // 
+            this.LabelSSID.AutoSize = true;
+            this.LabelSSID.Location = new System.Drawing.Point(12, 9);
+            this.LabelSSID.Name = "LabelSSID";
+            this.LabelSSID.Size = new System.Drawing.Size(30, 12);
+            this.LabelSSID.TabIndex = 0;
+            this.LabelSSID.Text = "SSID";
+            // 
+            // TextBoxSSID
+            // 
+            this.TextBoxSSID.Location = new System.Drawing.Point(48, 6);
+            this.TextBoxSSID.Name = "TextBoxSSID";
+            this.TextBoxSSID.Size = new System.Drawing.Size(362, 19);
+            this.TextBoxSSID.TabIndex = 1;
+            // 
+            // LabelKEY
+            // 
+            this.LabelKEY.AutoSize = true;
+            this.LabelKEY.Location = new System.Drawing.Point(16, 34);
+            this.LabelKEY.Name = "LabelKEY";
+            this.LabelKEY.Size = new System.Drawing.Size(26, 12);
+            this.LabelKEY.TabIndex = 2;
+            this.LabelKEY.Text = "KEY";
+            // 
+            // TextBoxKEY
+            // 
+            this.TextBoxKEY.Location = new System.Drawing.Point(48, 31);
+            this.TextBoxKEY.Name = "TextBoxKEY";
+            this.TextBoxKEY.Size = new System.Drawing.Size(362, 19);
+            this.TextBoxKEY.TabIndex = 3;
+            // 
+            // ButtonConnect
+            // 
+            this.ButtonConnect.Location = new System.Drawing.Point(178, 62);
+            this.ButtonConnect.Name = "ButtonConnect";
+            this.ButtonConnect.Size = new System.Drawing.Size(113, 31);
+            this.ButtonConnect.TabIndex = 4;
+            this.ButtonConnect.Text = "接続";
+            this.ButtonConnect.UseVisualStyleBackColor = true;
+            this.ButtonConnect.Click += new System.EventHandler(this.ButtonConnect_Click);
+            // 
+            // ButtonDisconnect
+            // 
+            this.ButtonDisconnect.Location = new System.Drawing.Point(297, 62);
+            this.ButtonDisconnect.Name = "ButtonDisconnect";
+            this.ButtonDisconnect.Size = new System.Drawing.Size(113, 31);
+            this.ButtonDisconnect.TabIndex = 4;
+            this.ButtonDisconnect.Text = "切断";
+            this.ButtonDisconnect.UseVisualStyleBackColor = true;
+            this.ButtonDisconnect.Click += new System.EventHandler(this.ButtonDisconnect_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(422, 105);
+            this.Controls.Add(this.ButtonDisconnect);
+            this.Controls.Add(this.ButtonConnect);
+            this.Controls.Add(this.TextBoxKEY);
+            this.Controls.Add(this.LabelKEY);
+            this.Controls.Add(this.TextBoxSSID);
+            this.Controls.Add(this.LabelSSID);
+            this.MaximizeBox = false;
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.ShowIcon = false;
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.Text = "NativeFifiSample";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label LabelSSID;
+        private System.Windows.Forms.TextBox TextBoxSSID;
+        private System.Windows.Forms.Label LabelKEY;
+        private System.Windows.Forms.TextBox TextBoxKEY;
+        private System.Windows.Forms.Button ButtonConnect;
+        private System.Windows.Forms.Button ButtonDisconnect;
     }
 }
 
--- NativeWifiSample/Form1.cs
+++ NativeWifiSample/Form1.cs
@@ -6,11 +6,16 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using System.Xml;
 
 namespace NativeWifiSample
 {
     public partial class Form1 : Form
     {
+        private IntPtr handle = IntPtr.Zero;
+        Guid InterfaceGuid;
+        //Guid ActiveInterfaceGuid;
+
         public Form1()
         {
             InitializeComponent();
@@ -18,79 +23,168 @@ namespace NativeWifiSample
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            IntPtr handle = IntPtr.Zero;
+            this.TextBoxSSID.Text = @"";
+            this.TextBoxKEY.Text = @"";
+
             uint _version;
 
-            try
+            if (WirelessAPI.WlanOpenHandle(2, IntPtr.Zero, out _version, out handle) != 0)
             {
-                if (WirelessAPI.WlanOpenHandle(2, IntPtr.Zero, out _version, out handle) != 0)
-                {
-                    return;
-                }
-
-                IntPtr ptr = new IntPtr();
-                if (WirelessAPI.WlanEnumInterfaces(handle, IntPtr.Zero, ref ptr) != 0)
-                {
-                    return;
-                }
-                WirelessAPI.WLAN_INTERFACE_INFO_LIST infoList = new WirelessAPI.WLAN_INTERFACE_INFO_LIST(ptr);
-                WirelessAPI.WlanFreeMemory(ptr);
-
-                Guid _guid = infoList.InterfaceInfo[0].InterfaceGuid;
-
-
-                string profileFormat =
-                    "<?xml version=\"1.0\" encoding=\"US-ASCII\"?>" +
-                    "<WLANProfile xmlns=\"http://www.microsoft.com/networking/WLAN/profile/v1\">\n" +
-                    "<name>Buffalo-G-F16E</name>\n" +
-                    "<SSIDConfig>\n" +
-                    "<SSID>\n" +
-                    "<name>Buffalo-G-F16E</name>\n" +
-                    "</SSID>\n" +
-                    "</SSIDConfig>\n" +
-                    "<connectionType>ESS</connectionType>\n" +
-                    "<connectionMode>auto</connectionMode>\n" +
-                    "<autoSwitch>false</autoSwitch>\n" +
-                    "<MSM>\n" +
-                    "<security>\n" +
-                    "<authEncryption>\n" +
-                    "<authentication>WPA2PSK</authentication>\n" +
-                    "<encryption>AES</encryption>\n" +
-                    "<useOneX>false</useOneX>\n" +
-                    "</authEncryption>\n" +
-                    "<sharedKey>\n" +
-                    "<keyType>passPhrase</keyType>\n" +
-                    "<protected>false</protected>\n" +
-                    "<keyMaterial>xxxxxxxxxx</keyMaterial>\n" +
-                    "</sharedKey>\n" +
-                    "</security>\n" +
-                    "</MSM>\n" +
-                    "</WLANProfile>\n";
-
-                WirelessAPI.WLAN_CONNECTION_PARAMETERS _param = new WirelessAPI.WLAN_CONNECTION_PARAMETERS();
-                
-                // OSでプロファイル作成済みであればこの設定で成功する
-                //_param.wlanConnectionMode = WirelessAPI.WLAN_CONNECTION_MODE.wlan_connection_mode_profile;
-                //_param.strProfile = "Buffalo-G-F16E"; // SSID
-                //_param.dot11BssType = WirelessAPI.DOT11_BSS_TYPE.dot11_BSS_type_infrastructure;
-                //_param.dwFlags = 0;
-
-                // 一時プロファイルで接続する場合
-                _param.wlanConnectionMode = WirelessAPI.WLAN_CONNECTION_MODE.wlan_connection_mode_temporary_profile;
-                _param.strProfile = profileFormat;
-                _param.dot11BssType = WirelessAPI.DOT11_BSS_TYPE.dot11_BSS_type_infrastructure;
-                _param.dwFlags = 0;
-
-                uint _result = WirelessAPI.WlanConnect(handle, ref _guid, ref _param, new IntPtr());
-
+                System.Diagnostics.Debug.WriteLine("[ERROR] WlanOpenHandle failed");
+                return;
             }
-            finally
+            System.Diagnostics.Debug.WriteLine("[INFO] WlanOpenHandle success");
+            System.Diagnostics.Debug.WriteLine(String.Format("[INFO] NegotiatedVersion = {0}", _version));
+
+            IntPtr ptr = new IntPtr();
+            if (WirelessAPI.WlanEnumInterfaces(handle, IntPtr.Zero, ref ptr) != 0)
             {
-                if (handle != IntPtr.Zero)
+                System.Diagnostics.Debug.WriteLine("[ERROR] WlanEnumInterfaces failed");
+                return;
+            }
+            WirelessAPI.WLAN_INTERFACE_INFO_LIST infoList = new WirelessAPI.WLAN_INTERFACE_INFO_LIST(ptr);
+            WirelessAPI.WlanFreeMemory(ptr);
+            System.Diagnostics.Debug.WriteLine("[INFO] WlanEnumInterfaces success");
+
+            //foreach (WirelessAPI.WLAN_INTERFACE_INFO _info in infoList.InterfaceInfo)
+            //{
+            //    if (_info.isState == WirelessAPI.WLAN_INTERFACE_STATE.wlan_interface_state_connected)
+            //    {
+            //        this.ActiveInterfaceGuid = _info.InterfaceGuid;
+            //    }
+            //}
+            this.InterfaceGuid = infoList.InterfaceInfo[0].InterfaceGuid;
+            System.Diagnostics.Debug.WriteLine(String.Format("[INFO] InterfaceGuid = {0}", this.InterfaceGuid));
+
+            WirelessAPI.WLAN_NOTIFICATION_SOURCE pdwPrevNotifSource;
+            WirelessAPI.WLAN_NOTIFICATION_CALLBACK _delegate = new WirelessAPI.WLAN_NOTIFICATION_CALLBACK(WlanNotificationCallback);
+
+            if (WirelessAPI.WlanRegisterNotification(this.handle, WirelessAPI.WLAN_NOTIFICATION_SOURCE.ACM, false,
+                _delegate, IntPtr.Zero, IntPtr.Zero, out pdwPrevNotifSource) != 0)
+            {
+                System.Diagnostics.Debug.WriteLine("[ERROR] WlanRegisterNotification failed");
+                return;
+            }
+            System.Diagnostics.Debug.WriteLine("[INFO] WlanRegisterNotification success");
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            if (handle != IntPtr.Zero)
+            {
+                if (WirelessAPI.WlanCloseHandle(handle, IntPtr.Zero) != 0)
                 {
-                    WirelessAPI.WlanCloseHandle(handle, IntPtr.Zero);
+                    System.Diagnostics.Debug.WriteLine("[ERROR] WlanCloseHandle failed");
+                    return;
                 }
+                System.Diagnostics.Debug.WriteLine("[INFO] WlanCloseHandle success");
             }
         }
+
+        private void ButtonConnect_Click(object sender, EventArgs e)
+        {
+            this.TextBoxSSID.Enabled = false;
+            this.TextBoxKEY.Enabled = false;
+            this.ButtonConnect.Enabled = false;
+            this.ButtonDisconnect.Enabled = false;
+
+            StringBuilder _profileXml = new StringBuilder();
+            XmlWriterSettings _profileXmlWriterSettings = new XmlWriterSettings();
+            //_profileXmlWriterSettings.Encoding = Encoding.ASCII;
+
+            XmlWriter _profileXmlWriter = XmlWriter.Create(_profileXml, _profileXmlWriterSettings);
+            _profileXmlWriter.WriteStartElement("WLANProfile", @"http://www.microsoft.com/networking/WLAN/profile/v1");
+            _profileXmlWriter.WriteElementString("name", this.TextBoxSSID.Text);
+
+            _profileXmlWriter.WriteStartElement("SSIDConfig");
+            _profileXmlWriter.WriteStartElement("SSID");
+            _profileXmlWriter.WriteElementString("name", this.TextBoxSSID.Text);
+            _profileXmlWriter.WriteEndElement(); // SSID
+            _profileXmlWriter.WriteEndElement(); // SSIDConfig
+
+            _profileXmlWriter.WriteElementString("connectionType", @"ESS");
+            _profileXmlWriter.WriteElementString("connectionMode", @"auto");
+            _profileXmlWriter.WriteElementString("autoSwitch", @"false");
+
+            _profileXmlWriter.WriteStartElement("MSM");
+            _profileXmlWriter.WriteStartElement("security");
+            _profileXmlWriter.WriteStartElement("authEncryption");
+            _profileXmlWriter.WriteElementString("authentication", @"WPA2PSK");
+            _profileXmlWriter.WriteElementString("encryption", @"AES");
+            _profileXmlWriter.WriteElementString("useOneX", @"false");
+            _profileXmlWriter.WriteEndElement(); // authEncryption
+
+            _profileXmlWriter.WriteStartElement("sharedKey");
+            _profileXmlWriter.WriteElementString("keyType", @"passPhrase");
+            _profileXmlWriter.WriteElementString("protected", @"false");
+            _profileXmlWriter.WriteElementString("keyMaterial", this.TextBoxKEY.Text);
+            _profileXmlWriter.WriteEndElement(); // sharedKey
+
+            _profileXmlWriter.WriteEndElement(); // security
+            _profileXmlWriter.WriteEndElement(); // MSM
+
+            _profileXmlWriter.WriteEndElement(); // WLANProfile
+
+            _profileXmlWriter.Close();
+
+            System.Diagnostics.Debug.WriteLine(_profileXml.ToString());
+
+            WirelessAPI.WLAN_CONNECTION_PARAMETERS _param = new WirelessAPI.WLAN_CONNECTION_PARAMETERS();
+
+            // OSでプロファイル作成済みであればこの設定で成功する
+            //_param.wlanConnectionMode = WirelessAPI.WLAN_CONNECTION_MODE.wlan_connection_mode_profile;
+            //_param.strProfile = "Buffalo-G-F16E"; // SSID
+            //_param.dot11BssType = WirelessAPI.DOT11_BSS_TYPE./*dot11_BSS_type_infrastructure*/;
+            //_param.dwFlags = 0;
+
+            // 一時プロファイルで接続する場合
+            _param.wlanConnectionMode = WirelessAPI.WLAN_CONNECTION_MODE.wlan_connection_mode_temporary_profile;
+            _param.strProfile = _profileXml.ToString();
+            _param.dot11BssType = WirelessAPI.DOT11_BSS_TYPE.dot11_BSS_type_infrastructure;
+            _param.dwFlags = 0;
+
+            if (WirelessAPI.WlanConnect(this.handle, ref this.InterfaceGuid, ref _param, new IntPtr()) != 0)
+            {
+                System.Diagnostics.Debug.WriteLine("[ERROR] WlanConnect failed");
+
+                this.TextBoxSSID.Enabled = true;
+                this.TextBoxKEY.Enabled = true;
+                this.ButtonConnect.Enabled = true;
+                this.ButtonDisconnect.Enabled = true;
+
+                return;
+            }
+            System.Diagnostics.Debug.WriteLine("[INFO] WlanConnect success");
+        }
+
+        private void WlanNotificationCallback(ref WirelessAPI.WLAN_NOTIFICATION_DATA notificationData, IntPtr context)
+        {
+            System.Diagnostics.Debug.WriteLine(String.Format("[INFO] WlanNotificationCallback : {0}",
+                Enum.GetName(typeof(WirelessAPI.WLAN_NOTIFICATION_CODE_ACM), notificationData.notificationCode)));
+            
+            if ((WirelessAPI.WLAN_NOTIFICATION_CODE_ACM)notificationData.NotificationCode ==
+                WirelessAPI.WLAN_NOTIFICATION_CODE_ACM.wlan_notification_acm_connection_complete)
+            {
+                this.Invoke((MethodInvoker)(()=>{
+                    this.TextBoxSSID.Enabled = true;
+                    this.TextBoxKEY.Enabled = true;
+                    this.ButtonConnect.Enabled = true;
+                    this.ButtonDisconnect.Enabled = true;
+                }));
+
+                System.Diagnostics.Debug.WriteLine(String.Format("[INFO] {0} connected", this.TextBoxSSID.Text));
+            }
+        }
+
+        private void ButtonDisconnect_Click(object sender, EventArgs e)
+        {
+            if (WirelessAPI.WlanDisconnect(this.handle, ref this.InterfaceGuid, IntPtr.Zero) != 0)
+            {
+                System.Diagnostics.Debug.WriteLine("[ERROR] WlanDisconnect failed");
+                return;
+            }
+            System.Diagnostics.Debug.WriteLine("[INFO] WlanDisconnect success");
+        }
+
     }
 }
